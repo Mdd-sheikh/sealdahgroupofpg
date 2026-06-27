668fbd238c04fa8661f2aa7daad5ff7259bbaeeb
Your Name <your-email@example.com>
2026-06-27 06:02:10 -0700

    add

--- frontend/pg/src/App.jsx
+++ frontend/pg/src/App.jsx
@@ -4,12 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
-import SecurityStandard from './pages/safty/Securitystandard'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage/LandingPage'
 import RoomPlans from './pages/Roomplan/Roomplan'
 import ComfortAndAmenities from './pages/LandingPage/facilate/ComfortAndAmenities'
 import ContactUs from './pages/contact/ContactUs'
+import SecurityStandard from './pages/safty/Securitystandard'
 
 function App() {
 
